ee4b0d21d1212964b744e9590b4d850c4a4fd1d8
yamaguchi <yamaguchi1001@icloud.com>
2022-02-25 23:58:59 +0900

    add logger

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,7 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.new(task_params)
     if @task.save
+      logger.debug "task: #{@task.attributes.inspect}"
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました"
     else
       render :new
