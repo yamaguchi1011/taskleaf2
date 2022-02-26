0546a52b10dbd58f32cdfcf76b1fe80040d4dc52
yamaguchi <yamaguchi1001@icloud.com>
2022-02-26 23:58:51 +0900

    add confirm_new

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     resources :users
   end
   root to: 'tasks#index'
-  resources :tasks
+  resources :tasks do
+    post :confirm, action: :confirm_new, on: :new
+  end
+  
 
 end
